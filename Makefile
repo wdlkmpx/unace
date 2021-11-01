wdlkmpx/unace

commit 656697c91c75be15ec12c91a83c8bbaa703f2294

remove source/base/all.c

Makefile
@@ -4,7 +4,6 @@ DEFINES		= -D__LINUX__ -D__unix__ -D__GCC__
 #DEFINES	= -D__unix__ -D__GCC__ -D__BIG_ENDIAN__ -D__EMULATE_UNALIGNED__
 
 BASE_ALL_CFILES =                      \
-	source/base/all.c                \
 	source/base/arcblk/arcblk.c      \
 	source/base/archives/ace/ace.c   \
 	source/base/archives/read/read.c \

source/base/all.c
@@ -1,4 +1,0 @@
-#define INCL_BASE_ACESTRUC_EXCLUSIVE
-#define INCL_BASE_ALL_EXCLUSIVE
-
-#include "base/includes.h"

source/base/acestruc.h
@@ -3,13 +3,6 @@
 
 #include "base/lfn/lfn.h"
 
-#ifdef INCL_BASE_ACESTRUC_EXCLUSIVE
-  #define DECLEXT
-#else
-  #define DECLEXT extern
-#endif
-
-
 #define BASE_ACESTRUC_EXTRACTVERSION 20
 
 #define BASE_ACESTRUC_HOST_MSDOS  0
@@ -243,8 +236,7 @@ typedef union {
 #define RESTOREALIGNMENT
 #include "base/packprag.h"
 
-DECLEXT PCHAR const
-          BASE_ACESTRUC_ACESIGN
+PCHAR const BASE_ACESTRUC_ACESIGN
 #ifdef INCL_BASE_ACESTRUC_EXCLUSIVE
   #ifdef USELIMITED
         = "USELTD"
@@ -254,8 +246,7 @@ DECLEXT PCHAR const
 #endif
 ;
 
-DECLEXT PCHAR const
-          BASE_ACESTRUC_EXTENSION
+PCHAR const BASE_ACESTRUC_EXTENSION
 #ifdef INCL_BASE_ACESTRUC_EXCLUSIVE
   #ifdef USELIMITED
         = ".acl"
@@ -265,8 +256,7 @@ DECLEXT PCHAR const
 #endif
 ;
 
-DECLEXT INT const
-          BASE_ACESTRUC_IsACL
+INT const BASE_ACESTRUC_IsACL
 #ifdef INCL_BASE_ACESTRUC_EXCLUSIVE
   #ifdef USELIMITED
         = 1
@@ -275,8 +265,5 @@ DECLEXT INT const
   #endif
 #endif
 ;
-
-
-#undef DECLEXT
 
 #endif

commit b36d7fea54e444092a51a07d06d1a67ee6727340

fix segfault
for some reason 656697c91c75be15ec12c91a83c8bbaa703f2294
introduced the segfault that I'm seeing now

but I don't recall seeing it in all the commits that followed

the codebase is still a total mess, even though it has been greatly simplified

Makefile
@@ -2,6 +2,7 @@ CPPFLAGS   += -Isource
 #DEFINES	= -D__EMULATE_UNALIGNED__
 
 BASE_ALL_CFILES =                      \
+	source/base/all.c      \
 	source/base/arcblk/arcblk.c      \
 	source/base/archives/ace/ace.c   \
 	source/base/archives/read/read.c \

source/base/all.c
@@ -1,0 +1,4 @@
+#define INCL_BASE_ACESTRUC_EXCLUSIVE
+#define INCL_BASE_ALL_EXCLUSIVE
+
+#include "base/includes.h"