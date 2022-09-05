81c8a38663bd578f8c7a58f5918dbfd97618c42b
wdlkmpx <wdlkmpx@gmail.com>
2022-09-05 19:17:04 +0800

    packprag.h: remove __MWERKS__ stuff

--- src/base/packprag.h
+++ src/base/packprag.h
@@ -10,18 +10,11 @@
   #if ( defined(__MACOSX__) || defined(__sun__) )
     #define RESTOREALIGNMENTOFF
     #pragma pack(4)
-  #elif defined(__WATCOMC__) || defined(__GNUC__) || defined(__MWERKS__) && (__dest_os == __win32_os)
+  #elif defined(__WATCOMC__) || defined(__GNUC__)
     #pragma pack(push, 4)
   #endif
-
-  #if defined(__MWERKS__) && (__dest_os == __mac_os)
-    #if !(__option(align_array_members))
-      #define RESTOREALIGNMENTOFF
-      #pragma align_array_members on
-    #endif
-  #endif
-
 #endif
+
 
 #ifdef SETNOALIGNMENT
   #undef SETNOALIGNMENT
@@ -29,18 +22,11 @@
   #if defined( __MACOSX__ ) || defined( __sun__ )
     #define RESTOREALIGNMENTON
     #pragma pack(1)
-  #elif defined(__WATCOMC__) || defined(__GNUC__) || defined(__MWERKS__) && (__dest_os == __win32_os)
+  #elif defined(__WATCOMC__) || defined(__GNUC__)
     #pragma pack(push, 1)
   #endif
-
-  #if defined(__MWERKS__) && (__dest_os == __mac_os)
-    #if (__option(align_array_members))
-      #define RESTOREALIGNMENTON
-      #pragma align_array_members off
-    #endif
-  #endif
-
 #endif
+
 
 #ifdef RESTOREALIGNMENT
   #undef RESTOREALIGNMENT
@@ -53,19 +39,7 @@
       #undef RESTOREALIGNMENTON
       #pragma pack(4)
     #endif
-  #elif defined(__WATCOMC__) || defined(__GNUC__) || defined(__MWERKS__) && (__dest_os == __win32_os)
+  #elif defined(__WATCOMC__) || defined(__GNUC__)
     #pragma pack(pop)
   #endif
-
-  #if defined(__MWERKS__) && (__dest_os == __mac_os)
-    #ifdef RESTOREALIGNMENTOFF
-      #undef RESTOREALIGNMENTOFF
-      #pragma align_array_members off
-    #endif
-    #ifdef RESTOREALIGNMENTON
-      #undef RESTOREALIGNMENTON
-      #pragma align_array_members on
-    #endif
-  #endif
-
 #endif
