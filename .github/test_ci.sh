ab388425559f57f9544bc5afa6455cd79ed4ed1d
wdlkmpx <wdlkmpx@gmail.com>
2022-12-28 10:08:43 +0800

    improve run-tests.sh

---
+++ .github/test_ci.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+if [ "$1" = "freebsd" ] ; then
+    #export REPO_AUTOUPDATE=NO
+    #pkg install -y ncurses
+    printf ""
+    exit $?
+fi
+
+if [ "$1" = "macos" ] ; then
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    ## macOS already provides ncurses by default
+    #brew install ncurses
+    exit $?
+fi
+
+# ============================================
+
+cmdecho()
+{
+    echo "---------------------"
+    echo "# $@"
+    echo "---------------------"
+    "$@"
+}
+
+
+print_config_log()
+{
+    for i in config.h config.log config.mk
+    do
+        echo "
+===============================
+	$i
+===============================
+"
+        cat ${i}
+    done
+}
+
+
+exit_error()
+{
+    print_config_log
+    exit 1
+}
+
+# ============================================
+
+uname -a
+
+export CFLAGS="-DDEBUG_W_ENDIAN"
+
+#cmdecho ./configure  || exit_error
+#cmdecho make         || exit_error
+#cmdecho make check   || exit_error
+#cmdecho make install || exit_error
+./run-tests.sh || exit_error
--- run-tests.sh
+++ run-tests.sh
@@ -12,11 +12,33 @@ mkdir -p "${TESTDIR}"
 #app="$(pwd)/src/unace1"
 app="$(pwd)/unace"
 appbn=$(basename $app)
-#export CFLAGS="-D${appbn}_TRACE -ggdb3"
+export CFLAGS="-D${appbn}_TRACE -ggdb3"
 test_acev1_dir=${MWD}/tests.ace.v1
 test_acev2_dir=${MWD}/tests.ace.v2
+help_ret_code=0
 
 # ===========================================================================
+
+ERROR_FILES=''
+add_error_file()
+{
+    ret=1
+    ERROR_FILES="$ERROR_FILES $1"
+}
+
+
+set_wine()
+{
+    wine=''
+    if [ "$(uname)" = "Linux" ] ; then
+        wine='wine'
+        if command -v wine >/dev/null ; then
+            echo "WINE is not installed, cannot test .exe binaries..."
+            exit 1
+        fi
+    fi
+}
+
 
 if test -z "$MD5SUM" ; then
 	if command -v md5sum 2>/dev/null ; then
@@ -27,6 +49,18 @@ if test -z "$MD5SUM" ; then
 		MD5SUM='md5'
 	fi
 fi
+
+
+if test -z "$MD5SUM" ; then
+	if command -v md5sum 2>/dev/null ; then
+		MD5SUM='md5sum'
+	elif command -v gmd5sum 2>/dev/null ; then
+		MD5SUM='gmd5sum'
+	elif command -v md5 2>/dev/null ; then
+		MD5SUM='md5'
+	fi
+fi
+
 
 check_md5()
 {
@@ -41,8 +75,8 @@ check_md5()
 		while read md5 file
 		do
 			if ! md5 -q -s "$md5" "$file" >/dev/null 2>&1 ; then
-				ret=1
 				echo "ERROR"
+				add_error_file ${logfile}
 				return
 			fi
 		done < ${md5file}
@@ -50,11 +84,11 @@ check_md5()
 		return
 	fi
 
-	if ${MD5SUM} -c ${md5file} >${logfile} 2>&1 ; then
+	if ${MD5SUM} -c ${md5file} >>${logfile} 2>&1 ; then
 		echo "OK"
 	else
-		ret=1
 		echo "ERROR"
+		add_error_file ${logfile}
 	fi
 }
 
@@ -75,8 +109,9 @@ echo
 #fi
 
 if test -f ${app}.exe ; then
-	# .exe binary - cross compiled
-	app="wine ${app}"
+	# .exe binary
+	set_wine
+	app="${wine} ${app}"
 elif test -f ${app} ; then
 	app="${app}"
 else
@@ -91,114 +126,173 @@ else
 	fi
 fi
 
+# basic check
+expected_ret=${help_ret_code}
+${app} -h >/dev/null
+iret=$?
+if [ ${iret} -ne ${expected_ret} ] ; then
+    echo "Something is wrong with $app"
+    echo "Got code $iret (expected ${expected_ret})"
+    echo "Aborted"
+    exit 1
+fi
+
 # ===========================================================================
+
+cmdecho()
+{
+    gdbargs=''
+    if command -v gdb >/dev/null ; then
+        gdbargs='gdb --args'
+    fi
+    echo "------------------------------------"
+    echo "${gdbargs} $@"
+    echo "------------------------------------"
+    "$@"
+}
 
 cd ${TESTDIR}
 
 ret=0
 
-echo
-
-## these 2 require some tweaks
-#printf "* tests/dirtraversal1.ace: "
-#${app} x -y ${test_acev1_dir}/dirtraversal1.ace >${TESTDIR}/dirtraversal1.log 2>&1
+## This requires some tweaks
+#ACEFILE=${test_acev1_dir}/dirtraversal1.ace
+#MD5FILE=
+#LOGFILE=${TESTDIR}/dirtraversal1.log
+#echo
+#printf "* tests/${ACEFILE##*/}: "
+#cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
 #if [ $? -eq 7 ] ; then
 #	echo "OK"
 #else
-#	ret=1
 #	echo "ERROR"
+#	add_error_file ${LOGFILE}
 #fi
 
-#printf "* tests/dirtraversal2.ace: "
-#${app} x -y ${test_acev1_dir}/dirtraversal2.ace >${TESTDIR}/dirtraversal2.log 2>&1
+## This requires some tweaks
+#ACEFILE=${test_acev1_dir}/dirtraversal2.ace
+#MD5FILE=
+#LOGFILE=${TESTDIR}/dirtraversal2.log
+#printf "* tests/${ACEFILE##*/}: "
+#cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
 #if [ $? -eq 7 ] ; then
 #	echo "OK"
 #else
-#	ret=1
 #	echo "ERROR"
+#	add_error_file ${LOGFILE}
 #fi
 
+ACEFILE=${test_acev1_dir}/out_of_bounds.ace
+MD5FILE=
+LOGFILE=${TESTDIR}/out_of_bounds.log
 printf "* tests/out_of_bounds.ace: "
-${app} t ${test_acev1_dir}/out_of_bounds.ace >${TESTDIR}/out_of_bounds.log 2>&1
-if grep 'fault' ${TESTDIR}/out_of_bounds.log ; then
+cmdecho ${app} t ${ACEFILE} >${LOGFILE} 2>&1
+if grep 'fault' ${LOGFILE} ; then
 	echo "ERROR"
+	add_error_file ${LOGFILE}
 else
 	echo "OK"
 fi
 
-printf "* tests/onefile.ace: "
+ACEFILE=${test_acev1_dir}/onefile.ace
+MD5FILE=${test_acev1_dir}/onefile.md5
+LOGFILE=${TESTDIR}/onefile.log
+printf "* tests/${ACEFILE##*/}: "
 rm -f CHANGES.LOG
-${app} x -y ${test_acev1_dir}/onefile.ace >${TESTDIR}/onefile.log 2>&1
-check_md5 ${test_acev1_dir}/onefile.md5 onefile.log
+cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
-printf "* tests/passwd.ace: "
+ACEFILE=${test_acev1_dir}/passwd.ace
+MD5FILE=${test_acev1_dir}/passwd.md5
+LOGFILE=${TESTDIR}/passwd.log
+printf "* tests/${ACEFILE##*/}: "
 rm -f passwd.m4
-${app} x -y -p1234 ${test_acev1_dir}/passwd.ace >${TESTDIR}/passwd.log 2>&1
-check_md5 ${test_acev1_dir}/passwd.md5 passwd.log
+cmdecho ${app} x -y -p1234 ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
-printf "* tests/ZGFX2.ace: "
+ACEFILE=${test_acev1_dir}/ZGFX2.ace
+MD5FILE=${test_acev1_dir}/ZGFX2.md5
+LOGFILE=${TESTDIR}/ZGFX2.log
+printf "* tests/${ACEFILE##*/}: "
 rm -rf ZGFX2
-${app} x -y ${test_acev1_dir}/ZGFX2.ace >${TESTDIR}/ZGFX2.log 2>&1
-if [ $? -eq 0 ] && [ $(find ZGFX2 -type f | wc -l) -eq 223 ] ; then
-	check_md5 ${test_acev1_dir}/ZGFX2.md5 ZGFX2.log
-else
-	ret=1
-	echo "ERROR"
-fi
+cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
-printf "* tests/zdir.ace: "
+ACEFILE=${test_acev1_dir}/zdir.ace
+MD5FILE=${test_acev1_dir}/zdir.md5
+LOGFILE=${TESTDIR}/zdir.log
+printf "* tests/${ACEFILE##*/}: "
 rm -rf zman
-${app} x -y ${test_acev1_dir}/zdir.ace >${TESTDIR}/zdir.log 2>&1
-if [ $? -eq 0 ] && [ $(find zman -type f | wc -l) -eq 20 ] ; then
-	check_md5 ${test_acev1_dir}/zdir.md5 zdir.log
-else
-	ret=1
-	echo "ERROR"
-fi
+cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
-printf "* tests/multivolume.ace: "
+ACEFILE=${test_acev1_dir}/multivolume.ace
+MD5FILE=${test_acev1_dir}/multivolume.md5
+LOGFILE=${TESTDIR}/multivolume.log
+printf "* tests/${ACEFILE##*/}: "
 rm -rf aclocal
-${app} x -y ${test_acev1_dir}/multivolume.ace >${TESTDIR}/multivolume.log 2>&1
-if [ $? -eq 0 ] && [ $(find aclocal -type f | wc -l) -eq 77 ] ; then
-	check_md5 ${test_acev1_dir}/multivolume.md5 multivolume.log
-else
-	ret=1
-	echo "ERROR"
-fi
+cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
-#==================================================================
+# ===========================================================================
 # v2
 
-printf "* tests2/dir2.ace: "
+ACEFILE=${test_acev2_dir}/dir2.ace
+MD5FILE=${test_acev2_dir}/dir2.md5
+LOGFILE=${TESTDIR}/dir2.log
+printf "* tests2/${ACEFILE##*/}: "
 rm -rf dir2
-${app} x -y ${test_acev2_dir}/dir2.ace >${TESTDIR}/dir2.log 2>&1
-if [ $? -eq 0 ] && [ $(find dir2 -type f | wc -l) -eq 35 ] ; then
-	check_md5 ${test_acev2_dir}/dir2.md5 dir2.log
-else
-	ret=1
-	echo "ERROR"
-fi
+cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
-printf "* tests2/multivol2.ace: "
+ACEFILE=${test_acev2_dir}/multivol2.ace
+MD5FILE=${test_acev2_dir}/multivol2.md5
+LOGFILE=${TESTDIR}/multivol2.log
+printf "* tests2/${ACEFILE##*/}: "
 rm -rf multivol2
-${app} x -y ${test_acev2_dir}/multivol2.ace >${TESTDIR}/multivol2.log 2>&1
-if [ $? -eq 0 ] && [ $(find multivol2 -type f | wc -l) -eq 37 ] ; then
-	check_md5 ${test_acev2_dir}/multivol2.md5 multivol2.log
-else
-	ret=1
-	echo "ERROR"
-fi
+cmdecho ${app} x -y ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
-printf "* tests2/passwd2.ace: "
+ACEFILE=${test_acev2_dir}/passwd2.ace
+MD5FILE=${test_acev2_dir}/passwd2.md5
+LOGFILE=${TESTDIR}/passwd2.log
+printf "* tests2/${ACEFILE##*/}: "
 rm -f passwd2.m4
-${app} x -y -p1234 ${test_acev2_dir}/passwd2.ace >${TESTDIR}/passwd2.log 2>&1
-check_md5 ${test_acev2_dir}/passwd2.md5 passwd2.log
+cmdecho ${app} x -y -p1234 ${ACEFILE} >${LOGFILE} 2>&1
+check_md5 ${MD5FILE} ${LOGFILE}
 
+# ===========================================================================
+# TODO: extra...
+
+#https://telparia.com/fileFormatSamples/archive/ace/131
+#https://telparia.com/fileFormatSamples/archive/ace/ALCHT.ACE
+#https://telparia.com/fileFormatSamples/archive/ace/AM101.ACE
+#https://telparia.com/fileFormatSamples/archive/ace/BJCRAPS.ACE
+#https://telparia.com/fileFormatSamples/archive/ace/BUGS.ACE
+#https://telparia.com/fileFormatSamples/archive/ace/JAZZ2.ACE
+#https://telparia.com/fileFormatSamples/archive/ace/Patch.ace
+#https://telparia.com/fileFormatSamples/archive/ace/beavis.ace
+#https://telparia.com/fileFormatSamples/archive/ace/crack.ace
+#https://telparia.com/fileFormatSamples/archive/ace/flgb12dk.ace
+#https://telparia.com/fileFormatSamples/archive/ace/karate.ace
+#https://telparia.com/fileFormatSamples/archive/ace/opawar2.ace
+#https://telparia.com/fileFormatSamples/archive/ace/scifip.ace
+#https://telparia.com/fileFormatSamples/archive/ace/stratego.ace
+#https://telparia.com/fileFormatSamples/archive/ace/trps.ace
 
 # ===========================================================================
 
+if [ $ret != 0 ] ; then
+    KEEP_TESTS=1 # errors, need logs
+    cat $ERROR_FILES
+fi
+
 if test -z "$KEEP_TESTS" ; then
 	rm -rf ${TESTDIR}
+else
+	printf "\n\n.......................................\n"
+	printf "- Logs are in $TESTDIR\n"
+	printf ".......................................\n\n"
 fi
 
 exit $ret
